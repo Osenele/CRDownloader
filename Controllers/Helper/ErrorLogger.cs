2ac752104b9b00785ae40bf1fc6aa1fb914121a6
Osenele Ikuenobe <Osenele.Ikuenobe@ng.ey.com>
2019-10-18 00:10:42 +0100

    Added error logging

--- Controllers/Helper/ErrorLogger.cs
+++ Controllers/Helper/ErrorLogger.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.IO;
+using System.Configuration;
 
 namespace CRDownloader.Controllers.Helper
 {
     public class ErrorLogger
     {
+        private static string LogFilePath = @ConfigurationManager.AppSettings["LogFilePath"];
         public static void LogException(Exception ex)
         {
-            using (StreamWriter file = new StreamWriter(""))
+            using (StreamWriter file = new StreamWriter(LogFilePath))
             {
                 file.WriteLine();
                 file.Close();
